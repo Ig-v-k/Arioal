d5732a04977783ed67006217c5d9ec8c4ccaf13c
Ihor Vysotskyi <lgorvysocky@gmail.com>
2021-07-05 13:09:31 +0200

    main secure logic

--- src/main/java/com/example/arioal/arioal/configuration/ApplicationFacesConfiguration.java
+++ src/main/java/com/example/arioal/arioal/configuration/ApplicationFacesConfiguration.java
@@ -7,7 +7,7 @@ import javax.security.enterprise.authentication.mechanism.http.LoginToContinue;
 import javax.security.enterprise.identitystore.DatabaseIdentityStoreDefinition;
 
 @DatabaseIdentityStoreDefinition(
-	  dataSourceLookup = "jdbc/emSecureAppArioalDS",
+	  dataSourceLookup = "jdbc/ArioalJDBCPostgresDS",
 	  callerQuery = "select user_password from users where username = ?",
 	  groupsQuery = "select user_group from users where username = ?"
 )
--- src/main/java/com/example/arioal/arioal/controller/LoginController.java
+++ src/main/java/com/example/arioal/arioal/controller/LoginController.java
@@ -1,9 +1,9 @@
 package com.example.arioal.arioal.controller;
 
+import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.security.enterprise.AuthenticationStatus;
@@ -17,7 +17,7 @@ import java.io.IOException;
 import java.io.Serializable;
 
 @Named
-@ViewScoped
+@RequestScoped
 public class LoginController implements Serializable {
   @NotEmpty
   private String username;
@@ -30,43 +30,43 @@ public class LoginController implements Serializable {
   SecurityContext securityContext;
 
   public void execute() throws IOException {
-    switch(processAuthentication()){
-      case SEND_CONTINUE:
-        facesContext.responseComplete();
-        break;
-      case SEND_FAILURE:
-        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Invalid Credentials", null));
-        break;
-      case SUCCESS:
-        getExternalContext().redirect(getExternalContext().getRequestContextPath() + "/home.xhtml");
-        break;
-    }
+	switch (processAuthentication()) {
+	  case SEND_CONTINUE:
+		facesContext.responseComplete();
+		break;
+	  case SEND_FAILURE:
+		facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Invalid Credentials", null));
+		break;
+	  case SUCCESS:
+		getExternalContext().redirect(getExternalContext().getRequestContextPath() + "/home.xhtml");
+		break;
+	}
   }
 
-  private AuthenticationStatus processAuthentication(){
-    ExternalContext ec = getExternalContext();
-    return securityContext.authenticate((HttpServletRequest)ec.getRequest(),
-          (HttpServletResponse)ec.getResponse(),
-          AuthenticationParameters.withParams().credential(new UsernamePasswordCredential(username, password)));
+  private AuthenticationStatus processAuthentication() {
+	ExternalContext ec = getExternalContext();
+	return securityContext.authenticate((HttpServletRequest) ec.getRequest(),
+		  (HttpServletResponse) ec.getResponse(),
+		  AuthenticationParameters.withParams().credential(new UsernamePasswordCredential(username, password)));
   }
 
-  private ExternalContext getExternalContext(){
-    return facesContext.getExternalContext();
+  private ExternalContext getExternalContext() {
+	return facesContext.getExternalContext();
   }
 
   public void setUsername(String username) {
-    this.username = username;
+	this.username = username;
   }
 
   public void setPassword(String password) {
-    this.password = password;
+	this.password = password;
   }
 
   public String getUsername() {
-    return username;
+	return username;
   }
 
   public String getPassword() {
-    return password;
+	return password;
   }
 }
