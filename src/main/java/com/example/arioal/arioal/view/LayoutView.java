b9e3748d0ef3c36d875b1af36a1ebba22f565768
Ihor Vysotskyi <lgorvysocky@gmail.com>
2021-07-10 15:48:13 +0200

    footer:

---
+++ src/main/java/com/example/arioal/arioal/view/LayoutView.java
@@ -0,0 +1,13 @@
+package com.example.arioal.arioal.view;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@SessionScoped
+public class LayoutView implements Serializable {
+  public void sendMessage() {
+    // TODO: 10.07.2021 - implement email sending
+  }
+}
